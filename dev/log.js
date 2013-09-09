63ead73a1f3a8be06f7c46af26a3bc5c9d9c1eb0
sam <289623783@qq.com>
2013-09-09 22:38:39 +0800

    打印级别
    增加：打印级别

--- dev/log.js
+++ dev/log.js
@@ -6,23 +6,40 @@
 sl.log = (function() {
 	var o = {};
 
+	var level = 0;
+
+	/**
+     * 打印日志信息
+     * @example SL.Log.i("mseeage") 或 SL.Log.info("mseeage");
+     * @method info
+     */
+    o.d = o.debug = function(msg) {
+    	if (level >= 0) {
+    		console.log("%c [DEBUG] " + msg, "background: #222; color: white");
+    	}
+    }
+
 	/**
      * 打印日志信息
      * @example SL.Log.i("mseeage") 或 SL.Log.info("mseeage");
      * @method info
      */
 	o.i = o.info = function(msg) {
-		console.log("%c" + msg, "background: #222; color: #bada55");
-	},
+		if (level >= 1) {
+			console.log("%c [INFO] " + msg, "background: #222; color: #bada55");
+		}
+	}
 
 	/**
      * 打印警告信息
      * @example SL.Log.w("mseeage") 或 SL.Log.warnning("message");
      * @method warnning
      */
-	o.w = o.warnning = function(msg) {
-		console.log("%c" + msg, "background: #222; color: orange");
-	},
+	o.w = o.warn = function(msg) {
+		if (level >= 2) {
+			console.log("%c [WARN] " + msg, "background: #222; color: orange");
+		}
+	}
 
 	/**
      * 打印错误信息
@@ -30,7 +47,19 @@ sl.log = (function() {
      * @method error
      */
 	o.e = o.error = function(msg) {
-		console.log("%c" + msg, "background: #222; color: red");
+		if (level >= 3) {
+			console.log("%c [ERROR] " + msg, "background: #222; color: red");
+		}
+	}
+
+	/**
+     * 设置打印日志级别，提供给开发者打印等级选择。
+     * @example
+     * @param {Number} num 打印级别，0 为仅打印 info，1 为打印 info，warnning，2 为打印 info，warnning，error。
+     * @method setLevel
+     */
+	o.setLevel = function(num) {
+		level = num;
 	}
 
 	return o;
